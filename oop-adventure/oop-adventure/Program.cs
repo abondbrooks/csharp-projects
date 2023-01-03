3c2e9db7d6446b1a1c9b03bd1ec7e4d881b91179
AZanae Bond-Brooks <abondbrooks@gmail.com>
2023-01-02 21:06:12 -0500

    Added a namespace for oop-adventure

--- oop-adventure/oop-adventure/Program.cs
+++ oop-adventure/oop-adventure/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, what is your name?");
+var name = Console.ReadLine();
+if (name == String.Empty)
+    name = "No Name";
+
+Console.WriteLine("Welcome {0} to OOP Adventure!", name);
+
