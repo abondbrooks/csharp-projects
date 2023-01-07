355bc92db60f0a56de1775d1817ae9315087de85
AZanae Bond-Brooks <abondbrooks@gmail.com>
2023-01-07 18:55:41 -0500

    C# Fundamentals
    For Loop

---
+++ ForIteration/ForIteration/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForIteration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 7)
+                {
+                    Console.WriteLine("Found Seven!");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
