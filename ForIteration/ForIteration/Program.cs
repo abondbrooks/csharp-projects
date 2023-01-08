b6bd80a172350d5dc1bba700108b8fa10a8429ca
AZanae Bond-Brooks <abondbrooks@gmail.com>
2023-01-08 18:32:48 -0500

    C# Fundamentals
    ForIteration

--- ForIteration/ForIteration/Program.cs
+++ ForIteration/ForIteration/Program.cs
@@ -11,13 +11,17 @@ namespace ForIteration
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
+/*            for (int i = 0; i < 10; i++)
             {
                 if (i == 7)
                 {
                     Console.WriteLine("Found Seven!");
                     break;
                 }
+            }*/
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
             }
             Console.ReadLine();
         }
