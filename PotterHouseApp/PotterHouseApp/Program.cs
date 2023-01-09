7a0fc32de10e373fbf80f28092020c9fbdac85ab
AZanae Bond-Brooks <abondbrooks@gmail.com>
2023-01-09 12:52:36 -0500

    The Potter House Game
    Personal project to practice using Methods, User Input & Variables

---
+++ PotterHouseApp/PotterHouseApp/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("The Potter House Game");
+
+Console.Write("What's your name? ");
+string name = Console.ReadLine();
+
+IntroduceYourself(name);
+Console.ReadLine();
+
+static void IntroduceYourself(string message)
+{
+    Console.WriteLine($"Welcome to The Potter House {message}.");
+}
+
+Console.Write("Pick a color: (1) Purple , (2) Yellow, (3) Green, (4) Black: ");
+int color = Console.ReadLine();
+
+
+static void PickAColor(string myColor)
+{
+    if (myColor == "1 ")
+}
