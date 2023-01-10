8d2d7f1b1e776a8205cf5fa6f1c1719d2088c400
AZanae Bond-Brooks <abondbrooks@gmail.com>
2023-01-10 09:56:59 -0500

    The Potter House Game
    updated user input to pick from an array

--- PotterHouseApp/PotterHouseApp/Program.cs
+++ PotterHouseApp/PotterHouseApp/Program.cs
@@ -7,31 +7,33 @@ IntroduceYourself(name);
 ContButnProced();
 Console.ReadLine();
 
-Console.Write("Pick a color: (1) Purple , (2) Yellow, (3) Green, (4) Black: ");
+string[] hogWartsHouses = new[] { "Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin" };
+
+Console.Write("Pick a color: (1) Blue , (2) Yellow, (3) Green, (4) Black: ");
 string color = Console.ReadLine();
 
-PickAColor(color,name);
+PickAColor(color,name,hogWartsHouses);
 Console.ReadLine();
 
 static void IntroduceYourself(string message)
 {
     Console.WriteLine($"Welcome to The Potter House {message}.");
 }
-static void PickAColor(string myColor, string myName)
+static void PickAColor(string myColor, string myName, string[] myArray)
 {
     if (myColor == "1")
-        Console.WriteLine($"Hufflepuff is your house {myName}.");
-
+        Console.WriteLine($"{myArray[2]} is your house {myName}.");
     else if (myColor == "2")
-        Console.WriteLine($"Griffindoor is your house {myName}.");
+        Console.WriteLine($"{myArray[0]} is your house {myName}.");
     else if (myColor == "3")
-        Console.WriteLine($"RavenClaw is your house {myName}.");
+        Console.WriteLine($"{myArray[3]} is your house {myName}.");
     else if (myColor == "4")
-        Console.WriteLine($"Slitherin is your house {myName}.");
+        Console.WriteLine($"{myArray[1]} is your house {myName}.");
     else
         Console.WriteLine("Invalid Entry. Exiting Program");
 }
 static void ContButnProced()
 {
-    Console.WriteLine("Press any key to continue...");
+    Console.WriteLine("Press enter key to continue...");
 }
+
