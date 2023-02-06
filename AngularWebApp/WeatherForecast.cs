5c14b1cf4fc32558fc5c438e2d6d004b09880f97
AZanae Bond-Brooks <abondbrooks@gmail.com>
2023-02-06 14:02:28 -0500

    ...

--- AngularWebApp/WeatherForecast.cs
+++ AngularWebApp/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace API;
+namespace AngularWebApp;
 
 public class WeatherForecast
 {
@@ -9,5 +9,4 @@ public class WeatherForecast
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
-
 }
