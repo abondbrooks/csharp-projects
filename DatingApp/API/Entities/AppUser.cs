0cf37c8820d13d61af09caa20928f87631128bf2
AZanae Bond-Brooks <abondbrooks@gmail.com>
2023-02-07 15:00:31 -0500

    added controllers and service extensions

--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
 namespace API.Entities
 {
     public class AppUser
@@ -6,7 +9,9 @@ namespace API.Entities
 
         public string UserName { get; set; }
 
+        public byte[] PasswordHash { get; set; }
 
-    
+        public byte[] PasswordSalt { get; set; }
+        
     }
 }
